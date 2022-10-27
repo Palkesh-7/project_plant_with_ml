a756cf76d2a463a33aed9ff7845cf6770083dbae
Palkesh-7 <palkeshmalviya.pm@gmail.com>
2022-10-27 23:01:48 +0530

    upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+from utils.disease import disease_dic
+print(disease_dic['Apple___Apple_scab'])
---
+++ test.py
@@ -0,0 +1,14 @@
+def fun(l):
+    l.sort()
+    print(l[-2])
+
+
+
+l =[]
+
+num = input("Enter any number")
+
+s = int(num)
+
+l.append(s)
+fun(l)
